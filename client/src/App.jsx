7998fb0c1e59eec991a1c450639144be942e070e
daoud <heh.gd42@gmail.com>
2023-02-01 22:16:26 +0200

    first push (finishis home page)

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import logo from "./assets/logo.svg";
+import CreatePost from "./pages/create_post/createPost.jsx";
+import Home from "./pages/home/home.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/create_post/createPost.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+
+function CreatePost() {
+  return <div>CreatePost</div>;
+}
+
+export default CreatePost;
